8ec60f16b823790cd118ee3f3fa39d1eb69bf05b
Rafael Yamada <rafaelyamada321@gmail.com>
2022-01-12 02:47:18 -0400

    validando permissoes no lado do servidor

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -22,7 +23,9 @@ export default function Dashboard() {
     <>
       <h1>Dashboard {user?.email}</h1>
 
-      { userCanSeeMetrics && <div>Métricas</div> }
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
